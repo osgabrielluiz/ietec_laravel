daac2f9a51b93733afb6a9717701212677bc47d3
Luiz Gabriel <91032465+osgabrielluiz@users.noreply.github.com>
2022-08-22 16:45:48 -0300

    arquivos -00

---
+++ resources/views/IETEC.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/teste.css">
+    <title>IETEC - Bem vindo!</title>
+</head>
+<body>
+    <header>
+        <div class="central_header">
+            <div class="menu">
+            <a href="#">Inicio</a>
+            <a href="/html/login.html">Login</a>
+            <a href="/html/quem_somos.html">Quem somos</a>
+            </div> <!--menu-->
+        </div><!--header-->
+    </header>
+    
+        <div class="texto_sobre">
+            <h1>Seja bem vindo a<br><span id="ietec">IETEC</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint facilis, 
+                <br>cum similique qui nam eaque temporibus autem perferendis, illo 
+                <br>asperiores deleniti soluta nobis totam ratione mollitia 
+                <br>repellat. Cupiditate, voluptate voluptatem?</p>
+                <button>Saiba mais</button>
+        </div>
+
+    <section class="sobre">
+        <div class="circulo">
+            <img id="money_icon" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets.webiconspng.com%2Fuploads%2F2017%2F01%2FMoney-Icon.png&f=1&nofb=1" alt="">
+        </div>
+    </section>
+
+    </body>
+</html>
