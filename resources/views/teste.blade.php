b28fe8bd3534cc20478ec84cb62ebc374fb4a123
Luiz Gabriel <91032465+osgabrielluiz@users.noreply.github.com>
2022-09-21 14:55:41 -0300

    footer

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -10,13 +10,13 @@
     <div class="container">
         <div class="row">
             <div class="footer-col">
-                <h4>Cefet-NF</h4>
+                <h4>-----</h4>
                 <ul>
                     <li>
-                        <a href="http://www.cefet-rj.br/index.php/apresentacao" target="blank">Apresentação</a>
-                        <a href="http://www.cefet-rj.br/index.php/direcao-geral" target="blank">Direção</a>
-                        <a href="http://www.cefet-rj.br/index.php/conselhos-2" target="blank">Conselhos</a>
-                        <a href="http://www.cefet-rj.br/index.php/ouvidoria" target="blank">Ouvidoria</a>
+                        <a href="#" target="blank">Apresentação</a>
+                        <a href="#" target="blank">Direção</a>
+                        <a href="#" target="blank">Conselhos</a>
+                        <a href="#" target="blank">Ouvidoria</a>
                     </li>
                 </ul>
              </div>
@@ -24,9 +24,9 @@
                  <h4>Sobre Nós</h4>
                  <ul>
                      <li>
-                         <a href="#" target="blank">Luiz Gabriel</a>
-                         <a href="#" target="blank">João Luiz</a>
-                         <a href="#" target="blank">Kauã</a>
+                         <a href="#" target="blank">-----</a>
+                         <a href="#" target="blank">-----</a>
+                         <a href="#" target="blank">-----</a>
                      </li>
                  </ul>
              </div>
@@ -34,7 +34,7 @@
                  <h4>Contato</h4>
                  <ul>
                      <li>
-                         <a href="https://www.instagram.com/youthandcode/" target="blank">Instagram</a>
+                         <a href="#" target="blank">Instagram</a>
                          <a href="#">Gmail</a>
                      </li>
                  </ul>
@@ -43,8 +43,8 @@
                  <h4>Páginas</h4>
                  <ul>
                      <li>
-                         <a href="index.html">Página Inicial</a>
-                         <a href="Jogo.html">Jogo</a>
+                         <a href="#">Página Inicial</a>
+                         <a href="#">Jogo</a>
                      </li>
                  </ul>
              </div>
