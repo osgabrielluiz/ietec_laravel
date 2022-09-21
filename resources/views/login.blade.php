9cc8a57601db482427085964d8dafb6d5fdb8c52
Jh0n16 <you@example.com>
2022-09-21 14:58:40 -0300

    Pagina de Login adicionada

---
+++ resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/public/css/login.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <form>
+        <fieldset>
+            <legend> <img src="perfil.png" alt=""> </legend>
+            <div class="login_area">
+                <input type="email" name="email" id="email" placeholder="Digite seu e-mail">
+            </div>
+            <div class="login_area">
+                <input type="password" name="passw" id="passw" placeholder="Digite sua Senha">
+            </div>
+            <div class="login_area">
+                <input type="submit" value="Logar" id="button">
+            </div>
+
+        </fieldset>
+    </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-Route::get('/quem somos', function (){
+Route::get('/quem_somos', function (){
     return view('/layouts/quem somos');
 });
 
 Route::get('/relatorio', function () {
     return view('/relatorio');
 });
+
+Route::get('/login', function () {
+    return view('/relatorio');
+})->name('login');
