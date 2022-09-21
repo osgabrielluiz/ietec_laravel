8fa57032e50f42abbc733c802bbb3e4020575320
Luiz Gabriel <91032465+osgabrielluiz@users.noreply.github.com>
2022-09-21 14:31:05 -0300

    Update teste.blade.php

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -6,38 +6,49 @@
 @endsection
 
 @section('footer')
-    
-<footer>
-    <!-- Rodapé -->
-      <div class="rodape">
-          <div class="contato">
-              
-              <!-- Telefone -->
-              <h3 class="htext">Contato</h3>
-              <p>Telefone: (21) 2569-4403</p>
-              <p>E-mail: ietec@cefet-rj.br</p>
-              
-          </div>
-  
-          <!-- Redes Sociais -->
-          <div class="redes_sociais">
-              <h3 class="htext">Mídias Sociais</h3>
-              <br>
-              <a href="https://pt-br.facebook.com/ieteccefet/" class="social" target="_blank">Facebook</a>
-              <br><br>
-              <a href="https://www.linkedin.com/in/ieteccefetrj/" class="social" target="_blank">Linkedin</a>
-          </div>
-          
-          <!-- Endereço -->
-          <div class="endereco">
-              <h3 class="htext">Endereço</h3>
-  
-              <p class="paragrafos">Rua General Canabarro, 485 - Bloco C - Térreo - Maracanã, RJ</p>
-              <p class="paragrafos">CEP: 20271-204 </p>
-              <p class="paragrafos">Aberto das 10 às 17 horas.</p>
-  
-          </div>
-      </div>
-  </footer> 
-
+<footer >
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>Cefet-NF</h4>
+                <ul>
+                    <li>
+                        <a href="http://www.cefet-rj.br/index.php/apresentacao" target="blank">Apresentação</a>
+                        <a href="http://www.cefet-rj.br/index.php/direcao-geral" target="blank">Direção</a>
+                        <a href="http://www.cefet-rj.br/index.php/conselhos-2" target="blank">Conselhos</a>
+                        <a href="http://www.cefet-rj.br/index.php/ouvidoria" target="blank">Ouvidoria</a>
+                    </li>
+                </ul>
+             </div>
+             <div class="footer-col">
+                 <h4>Sobre Nós</h4>
+                 <ul>
+                     <li>
+                         <a href="#" target="blank">Luiz Gabriel</a>
+                         <a href="#" target="blank">João Luiz</a>
+                         <a href="#" target="blank">Kauã</a>
+                     </li>
+                 </ul>
+             </div>
+             <div class="footer-col"> 
+                 <h4>Contato</h4>
+                 <ul>
+                     <li>
+                         <a href="https://www.instagram.com/youthandcode/" target="blank">Instagram</a>
+                         <a href="#">Gmail</a>
+                     </li>
+                 </ul>
+             </div>
+             <div class="footer-col">
+                 <h4>Páginas</h4>
+                 <ul>
+                     <li>
+                         <a href="index.html">Página Inicial</a>
+                         <a href="Jogo.html">Jogo</a>
+                     </li>
+                 </ul>
+             </div>
+         </div>
+     </div>   
+</footer>
 @endsection
